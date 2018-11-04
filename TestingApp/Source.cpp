165b1d69f5aba5f0ead31709b3c7af594e9b681f
Anton94_home <antonvdudov@gmail.com>
2018-11-04 20:40:44 +0200

    Add more tests

--- TestingApp/Source.cpp
+++ TestingApp/Source.cpp
@@ -6,6 +6,9 @@
 namespace MH
 {
 
+// All tests are testing the median of a sequence of numbers.
+// The median is calculated after each insertion of a number from the sequence.
+
 void ExecuteSequenceOfNumberInsertionAndMedianCalculationTest(const std::vector<int>& numbers, const std::vector<float>& expectedMedians)
 {
 	REQUIRE(numbers.size() == expectedMedians.size());
@@ -101,5 +104,38 @@ TEST_CASE("Sequence of numbers with only two values (successively)")
 
 	ExecuteSequenceOfNumberInsertionAndMedianCalculationTest(numbers, expectedMedians);
 }
+
+TEST_CASE("Sequence of sequentional numbers increasing order")
+{
+	ExecuteSequenceOfNumberInsertionAndMedianCalculationTest(
+		std::vector<int>{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+		std::vector<float>{ 0.f, 0.5f, 1.f, 1.5f, 2.f, 2.5f, 3.f, 3.5f, 4.f, 4.5f, 5.f }
+	);
+}
+
+TEST_CASE("Sequence of sequentional numbers decreasing order")
+{
+	ExecuteSequenceOfNumberInsertionAndMedianCalculationTest(
+		std::vector<int>{ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 },
+		std::vector<float>{ 0.f, 0.5f, 1.f, 1.5f, 2.f, 2.5f, 3.f, 3.5f, 4.f, 4.5f, 5.f }
+	);
+}
+
+TEST_CASE("Forcing the median holder structure's halves to reorganize #000")
+{
+	ExecuteSequenceOfNumberInsertionAndMedianCalculationTest(
+		std::vector<int>{   1,   2,    3,   9,    9,   9,   9,   0,   -1,  -2,   -3,  -4,   -5,  -6,   -7,  -8 },
+		std::vector<float>{ 1.f, 1.5f, 2.f, 2.5f, 3.f, 6.f, 9.f, 6.f, 3.f, 2.5f, 2.f, 1.5f, 1.f, 0.5f, 0.f, -0.5f }
+	);
+}
+
+
+/*
+	TODO: add fuzz tests.
+	Generate an array of random numbers.
+	Use sorting to calculate the median 'until' each position.
+	Run the test.
+	If the test fails, log the seed to be able to regenerate the problematic sequence.
+*/
 
 }
